c756de9186ae56c47ef8b2c955685e16ef90853e
jasonlenis <jasonsrenlenis@outlook.com>
2025-11-10 15:35:21 +0800

    Delete api.php

--- api.php
+++
@@ -1,9 +0,0 @@
-<?php
-$id  = $_GET['id'];
-$cmd = $_GET['cmd'];
-$file = 'users.json';
-$json = json_decode(file_get_contents($file), true);
-$json[$id]['cmd'] = $cmd;
-file_put_contents($file, json_encode($json, JSON_PRETTY_PRINT));
-echo "OK";
-?>
